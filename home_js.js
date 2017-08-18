129a0ab6f2affb6ab7a79797e3f1c954ce99a887
Vibhor <vibhorporwal99@gmail.com>
2017-08-19 01:17:20 +0530

    Add files via upload

---
+++ home_js.js
@@ -0,0 +1,18 @@
+
+function redirect(){
+var url = "contact.html";
+   window.location(url);}
+
+function abfun()
+{
+window.location("#wwww");
+
+}
+function homefun()
+{
+window.location("#ccc");
+
+}
+
+
+
